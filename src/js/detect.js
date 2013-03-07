7bb0d6a71a2947f75d29ac0db93911b18278092f
Denis <swainet@126.com>
2013-03-07 13:48:48 +0800

    update

--- src/js/detect.js
+++ src/js/detect.js
@@ -1,18 +1,20 @@
 define(function (require, exports, module) {
 
-  var ua = navigator.userAgent, os = {}, browser = {},
+  var ua = navigator.userAgent, os = {}, browser = {}, support = {},
     iphone = ua.match(/iPhone\sOS\s([\d_]+)/),
     ipad = ua.match(/(iPad).*OS\s([\d_]+)/),
     android = ua.match(/Android\s+([\d.]+)/),
     windowsphone = ua.match(/Windows\sPhone(?:\sOS)?\s([\d.]+)/),
-    ie = ua.match(/IEMobile\/([\d.]+)/);
+    ie = ua.match(/IEMobile\/([\d.]+)/),
+    touch = "ontouchend" in document;
 
   iphone && (os.ios = true, os.version = iphone[1].replace(/_/g, '.'));
   ipad && (os.ios = true, os.version = ipad[1].replace(/_/g, '.'));
   android && (os.android = true, os.version = android[1]);
   windowsphone && (os.windowsphone = true, os.version = windowsphone[1]);
+  //browser
   ie && (browser.ie = true, browser.version = ie[1]);
-  os.mobile = (iphone || ipad || android || windowsphone);
-
-  return {os: os, browser: browser};
+  //support
+  touch && (support.touch = true);
+  return {os: os, browser: browser, support: support};
 });
